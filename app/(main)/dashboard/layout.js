d8772a17801bbce3a8256d3ac79291cdb5414d61
HarshDobariya10 <dobariyaharsh93@gmail.com>
2025-05-13 22:57:36 +0530

    Edited

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Layout({ children }) {
   return (
-    <div className="px-5">
+    <div className="px-10">
       <div className="flex items-center justify-between mb-5">
         <h1 className="text-3xl font-bold md:text-5xl lg:text-5xl xl:text-5xl bg-gradient-to-b from-gray-500 via-gray-200 to-gray-500 text-transparent bg-clip-text">Industry Insights</h1>
       </div>
--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,11 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+    generateIndustryInsights, // This function is used to generate industry insights using Google Generative AI every WEEK
   ],
 });
